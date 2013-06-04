2620f56fd8e339c99614196b1215024521b41b68
andrewcwright <acw462@gmail.com>
2013-06-04 10:45:03 -0500

    fix renaming issue

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -1,15 +1,8 @@
 require 'test_helper'
 
 class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
-  test "browse articles" do
-    Post.create!(title: "First Post", body: "This is the first post")
-    Post.create!(title: "Second Post", body: "This is the second post")
-    assert_equal 2, Post.count
-    visit '/posts'
-    assert_equal 2, Post.count
-    assert_include page.body, "All Posts"
-    assert_include page.body, "First Post"
-    assert_include page.body, "Second Post"
+  test "posting a new article" do
+
   end
 
 end
